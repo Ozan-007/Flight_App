961a83c2a9b4b544f54a086aa1318aea61f60560
Ozan <ozan28061@gmail.com>
2021-12-31 20:19:26 +0100

    requirements.txt and decouple added

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=#6%c600xm0#vpmusyaiss*xczzhn3@zvky#x#&=q0=^eto_h'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
