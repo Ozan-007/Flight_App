90c1854da8a5207f5232b3cd381d62113d2e44a0
Ozan <ozan28061@gmail.com>
2022-01-01 20:16:55 +0100

    Year and month parts added by using upload_to

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,8 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
     bio = models.TextField(max_length=350)
     location = models.CharField(max_length=200)
-    photo = models.ImageField(null=True,blank=True)
+    photo = models.ImageField(null=True,blank=True, upload_to="profile-pictures/%Y/%m/") # upload to part is going to create another folder which has year and months in it.
+    # The url extension of photo is going to be like this /media/profile-pictures/2022/01 
 
     def save(self, *args, **kwargs):
         #Image Resize
--- core/settings.py
+++ core/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'uploads' 
+MEDIA_URL = '/media/' # This part will be added to url
+MEDIA_ROOT = 'uploads' # This one will create a file named uploads and images will be uploaded there.
 
 
 # Default primary key field type
