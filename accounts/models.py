1504d8e8b2745246db36e337df6e832461ad00b6
Ozan <ozan28061@gmail.com>
2022-01-01 19:30:11 +0100

    media root,media url and urls.py static part added

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Account(models.Model):
@@ -8,6 +9,16 @@ class Account(models.Model):
     bio = models.TextField(max_length=350)
     location = models.CharField(max_length=200)
     photo = models.ImageField(null=True,blank=True)
+
+    def save(self, *args, **kwargs):
+        #Image Resize
+        super().save(**args, **kwargs)
+        if self.photo:
+            img = Image.open(self.photo.path)
+            if img.height > 600 or img.width > 600:
+                output_size = (600,600)
+                img.thumbnail(output_size)
+                img.save(self.photo.path)
 
 
     def __str__(self):
--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'uploads' 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
