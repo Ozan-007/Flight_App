ad5c9b55ddb8c0bfb4c169b70bfe2432404afb0e
Ozan <ozan28061@gmail.com>
2022-01-02 16:45:35 +0100

    serializer created, views created added to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.urls.conf import include
 
+from accounts.views import ListCreateAccountView, RetrieveUpdateDestroyAccountView
+
 urlpatterns = [
+    path('', ListCreateAccountView.as_view(), name="List & Create"),
+    path('<int:pk>', RetrieveUpdateDestroyAccountView.as_view(), name="Retrieve Update Delete"),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions
+from accounts.models import Account
 
+from accounts.serializers import AccountSerializer
 # Create your views here.
+
+class ListCreateAccountView(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class RetrieveUpdateDestroyAccountView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAdminUser]
+    
+
