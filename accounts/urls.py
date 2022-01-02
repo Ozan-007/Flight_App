04409badbbc9c40ce0308c2790af0f86d8392776
Ozan <ozan28061@gmail.com>
2022-01-02 16:17:13 +0100

    registration path added to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
