cf849b9bf30ff63c49ddba27b6fe4d791de249c5
Ozan <ozan28061@gmail.com>
2022-01-05 20:40:18 +0100

    Flight models added

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flight_number = models.IntegerField()
+    airline = models.CharField(max_length=250)
+    departure_city = models.CharField(max_length=250)
+    arrival_city = models.CharField(max_length=250)
+    departure_date = models.DateField()
+    estimatedTimeofDeparture = models.TimeField()
+
+    def __str__(self):
+        return self.departure_city + " " + self.arrival_city + " " + self.flight_number 
+
+        
