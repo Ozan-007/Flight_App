24f5d696e06bc12c2793ba48ee1ae8d6c749c25d
Ozan <ozan28061@gmail.com>
2022-01-10 18:31:56 +0100

    Passenger Reservation serializer created

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Reservation,Flight,Passenger
 
@@ -7,4 +8,21 @@ class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = "__all__"
+
+
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = "__all__"
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+        passenger = PassengerSerializer(many=True, required=False)
+        flight_id = serializers.IntegerField()
+        user = serializers.StringRelatedField()
+        user_id = serializers.IntegerField(required=False, write_only=True)        
+        class Meta:
+            model = Reservation
+            fields = ("id", "flight_id","passenger","user","user_id")
+
 
