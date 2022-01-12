5b27eafabf70979a82c2393661fc5759b8a364d5
Ozan <ozan28061@gmail.com>
2022-01-12 16:59:54 +0100

    filter.SearchFilter added to FlightListView

--- flights/views.py
+++ flights/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
-from rest_framework import permissions
-
 from flights.serializers import FlightSerializer, ReservationSerializer
 from .models import Flight, Reservation
 from rest_framework.permissions import IsAdminUser
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets , filters
 from .permissions import IsStuffOrReadOnly
+
 # Create your views here.
 
 class FlightListView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = [IsStuffOrReadOnly,]
-
+    filter_backends =  [filters.SearchFilter]
+    search_fields = ["departure_city", 'arrival_city']
 
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
